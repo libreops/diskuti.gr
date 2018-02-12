156f7ce4b6e0202ba999d36b928b4bc6de8d0429
Nikos Roussos <nikos@roussos.cc>
2018-02-12 17:42:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# diskuti.gr
+
+This is the source code of [diskuti.gr](https://diskuti.gr). A service providing online communication tools for grassroots communities.
+
+## Join
+
+[![irc](https://img.shields.io/badge/Matrix-%23hsgr:matrix.org-blue.svg)](https://riot.im/app/#/room/#hsgr:matrix.org)
+[![irc](https://img.shields.io/badge/IRC-%23hsgr%20on%20freenode-blue.svg)](https://webchat.freenode.net/?channels=hsgr)
+
+## License
+
+[![license](https://img.shields.io/badge/license-AGPL%203.0-6672D8.svg)](LICENSE)
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+/* global require */
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var stylelint = require('gulp-stylelint');
+
+var lintPathsJS = [
+    'static/js/*.js',
+    'gulpfile.js'
+];
+
+var lintPathsCSS = [
+    'static/css/*.css'
+];
+
+gulp.task('js:lint', () => {
+    return gulp.src(lintPathsJS)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('css:lint', () => {
+    return gulp.src(lintPathsCSS)
+        .pipe(stylelint({
+            reporters: [{ formatter: 'string', console: true}]
+        }));
+});
+
+gulp.task('assets', function(){
+    var p = require('./package.json');
+    var assets = p.assets;
+    return gulp.src(assets, {cwd : 'node_modules/**'})
+        .pipe(gulp.dest('static/lib'));
+});
+
+gulp.task('test', function(){
+    gulp.start('js:lint');
+    gulp.start('css:lint');
+});
+
+gulp.task('default', function() {
+    gulp.start('assets');
+    gulp.start('test');
+});
